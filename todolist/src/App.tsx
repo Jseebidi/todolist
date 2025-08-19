200f6d9e42503ab430898028afa461a884e3feac
Jseebidi <ong.375007@novaliches.sti.edu.ph>
2025-08-19 17:55:57 +0800

    added tailwind

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,13 +15,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite and React Typescript</h1>
+      <h1 className = "text-4xl">Vite and React Typescript</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          more active on: <code>github </code> Jseebidi
+          more active on: github Jseebidi
+        <br></br>
+          hmu ig: sxyyyyyy.__
         </p>
       </div>
       <p className="read-the-docs">
